81eb058ea7624d3423c75b692cee15ef92d25104
roee ltmanovich <roeeli770@gmail.com>
2024-11-14 17:56:13 +0200

    create emit to socet

--- src/components/pages/organization/Details.tsx
+++ src/components/pages/organization/Details.tsx
@@ -1,34 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../../redux/store";
 import { fetchDetails } from "../../../redux/slise/organizationSlice";
+import { socket } from "../../../main";
 
 export default function Details() {
-    const user = useAppSelector((state) => state.organization.user);
+    const organization = useAppSelector((state) => state.organization.user);
+    const user = useAppSelector((state) => state.user.user);
+
     const dispatch = useAppDispatch();
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+    const [loction, setloction] = useState("");
+    const [missile, setMissile] = useState("");
 
     useEffect(() => {
         dispatch(fetchDetails());
-        console.log(user);
+        console.log(organization);
     }, []);
 
+    const hendelNewAttack = ()=>{
+
+        console.log(user);
+        
+        socket.emit("sendAttackMissile",{loction, missile, id_user: user.user_id, username: user?.username})
+    }
 
     return (
         <div>
-            <p>{user?.username}</p>
-            {user?.detailsOnOrganization.resources.map((reso) => {
-                return <p key={reso.name}>{`${reso.name}: ${reso.amount}`}</p>;
+            <p>{organization?.username}</p>
+            {organization?.detailsOnOrganization.resources.map((reso) => {
+                return <button onClick={()=>{setMissile(reso.name)}} key={reso.name}>{`${reso.name}: ${reso.amount}`}</button>;
             })}
-            {}
-            <select>
-                <option value="IDF - North">North</option>
-                <option value="IDF - West Bank">West Bank</option>
-                <option value="IDF - Center">Center</option>
-                <option value="IDF - South">South</option>
-            </select>
-            <button>Send missile</button>
-            
+            {!organization?.detailsOnOrganization.name.includes("IDF")?
+            <div>
+                <select onChange={(e) => {setloction(e.target.value)}}>
+                    <option value="IDF - North">North</option>
+                    <option value="IDF - West Bank">West Bank</option>
+                    <option value="IDF - Center">Center</option>
+                    <option value="IDF - South">South</option>
+                </select>
+                <button onClick={hendelNewAttack}>Send missile</button>
+            </div>:
+            null }
         </div>
     );
 }
