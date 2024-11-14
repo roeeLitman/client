817101aa3d10b7d9312d290bb9e0ce41cd0bc6bd
roee ltmanovich <roeeli770@gmail.com>
2024-11-14 11:31:53 +0200

    add 2 roter to app for login and register

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Login from "./components/authpage/Login";
+import Register from "./components/authpage/Register";
 
 export default function App() {
-  return (
-    <div>App</div>
-  )
+    return (
+        <div>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/" element={<Navigate to={"/login"} />} />
+            </Routes>
+        </div>
+    );
 }
